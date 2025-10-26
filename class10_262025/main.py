72c3887054deea12fcf1c412477ec4af4842c96c
Jahanzaib Tayyab <jtayyab204@gmail.com>
2025-10-26 20:53:29 +0500

    Revise Assignment 06: Data Types, Operators & Strings to enhance examples and clarify explanations

---
+++ class10_262025/main.py
@@ -0,0 +1,8 @@
+import myfile
+import pandas as pd
+print("Hello, World! crtdyvubhqwjvgsvgcrhef")
+print(myfile.a)
+print(myfile.b)
+print(myfile.list1)
+print(myfile.add())
+#print(pd.)
---
+++ class10_262025/myfile.py
@@ -0,0 +1,12 @@
+a=50
+b=100
+
+list1=[1,2,3,4,5]
+
+def add():
+    print(a+b)
+    return "`Hello, World!`"
+    print(Python)
+
+def abccv():
+    print("Hello, World!")
